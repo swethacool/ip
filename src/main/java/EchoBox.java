62bc1f730935b366c40e0b32b2a072c1778a00af
swethacool <swetha2294cool@gmail.com>
2025-02-03 22:29:58 +0800

    Level 1 - Echo

--- src/main/java/EchoBox.java
+++ src/main/java/EchoBox.java
@@ -2,11 +2,25 @@ import java.util.Scanner;
 
 public class EchoBox {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println(" 🤖 Hello! I'm EchoBox!");
-        System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("🤖 Hello! I'm EchoBox!");
+        System.out.println("😊 How can I help you today?");
+
+        while (true) {
+            String userInput = scanner.nextLine();
+
+            if (userInput.equalsIgnoreCase("bye")) {
+                System.out.println("EchoBox: 👋 Bye! Hope to see you again soon! 🚀");
+                break;
+            } else if (userInput.equalsIgnoreCase("hello")) {
+                System.out.println("EchoBox: " + userInput);
+                System.out.println("EchoBox: Hey there! 😊 How’s your day?");
+            } else if (userInput.equalsIgnoreCase("sad")) {
+                System.out.println("EchoBox: " + userInput);
+                System.out.println("EchoBox: Don't be sad! Here's a hug 🤗");
+            } else {
+                System.out.println("EchoBox: " + userInput);
+            }
+        }
     }
 }
